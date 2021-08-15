e4c21df8e773a40156c706898a5a7b08bfe32c2d
PhamChien0611 <chienpdgch190396@fpt.edu.vn>
2021-08-15 17:17:44 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,163 @@
+const EXPRESS = require('express')
+const path = require('path');
+const { Int32 } = require('mongodb');
+const hbs = require('hbs')
+const session = require('express-session');
+const bodyParser = require('body-parser')
+
+const { addProduct, deleteProduct, searchProduct, getAllProducts, getProductById, updateProduct, getAllAccounts, addAccount, deleteAccount, getAccountById, updateAccount, checkUser } = require('./databaseHandler');
+const { Console } = require('console');
+
+const APP = EXPRESS()
+
+APP.use(bodyParser.urlencoded({ extended: false }));
+
+APP.use(session({
+    resave: true,
+    saveUninitialized: true,
+    secret: 'abcc##$$0911233$%%%32222',
+    cookie: { maxAge: 60000 }
+}));
+
+APP.use(EXPRESS.urlencoded({ extended: true }))
+
+APP.set('view engine', 'hbs');
+APP.set('views', path.join(__dirname, 'views'))
+
+// Begin : Manage Product
+//Search by ID to edit Product Function
+APP.get('/editProduct', async (req, res) => {
+    const idInput = req.query.id;
+    const username = req.session.username;
+    const search_Product = await getProductById(idInput);
+    res.render('editProduct', { loginName: username, product: search_Product })
+})
+
+//Update product function
+APP.post('/updateProduct', async (req, res) => {
+    const id = req.body.id;
+    const nameInput = req.body.txtName;
+    const priceInput = req.body.txtPrice;
+    const imgURLInput = req.body.imgURL;
+    await updateProduct(id, nameInput, priceInput, imgURLInput);
+    res.redirect('manageProducts');
+})
+
+//Add product Function
+APP.post('/doAddProduct', async (req, res) => {
+    const nameInput = req.body.txtName;
+    const priceInput = req.body.txtPrice;
+    const imgURLInput = req.body.imgURL;
+    const newProduct = { name: nameInput, price: Int32(priceInput), imgURL: imgURLInput };
+    await addProduct(newProduct);
+    res.redirect('manageProducts');
+})
+
+APP.get('/addProduct', (req, res) => {
+    const username = req.session.username;
+    res.render('addProduct', {loginName : username})
+})
+
+//Manage Products
+APP.get('/manageProducts', async (req, res) => {
+    const username = req.session.username;
+    const allProducts = await getAllProducts();
+    res.render('manageProducts', { loginName: username, data: allProducts })
+})
+
+//Delete product Function
+APP.get('/delete', async (req, res) => {
+    const idInput = req.query.id;
+    await deleteProduct(idInput);
+    res.redirect('manageProducts');
+})
+
+//Home Page
+APP.get('/home', async (req, res) => {
+    const allProducts = await getAllProducts();
+    const username = req.session.username;
+    res.render('home', { loginName: username, data: allProducts })
+})
+
+//Search product Funtion
+APP.post('/search', async (req, res) => {
+    const searchInput = req.body.txtSearch;
+    const username = req.session.username;
+    const allProducts = await searchProduct(searchInput);
+    res.render('manageProducts', { loginName: username, data: allProducts })
+})
+
+// End: Manage Product
+
+
+// Begin : Manage Account
+
+//Get All Accounts
+APP.get('/manageAccounts', async (req, res) => {
+    const allAccounts = await getAllAccounts();
+    const username = req.session.username;
+    res.render('manageAccounts', { loginName: username, data: allAccounts })
+})
+
+//Add product Function
+APP.post('/doAddAccount', async (req, res) => {
+    const usernameInput = req.body.txtUsername;
+    const passwordInput = req.body.txtPassword;
+    const newAccount = { username: usernameInput, password: passwordInput };
+    await addAccount(newAccount);
+    res.redirect('manageAccounts');
+})
+
+APP.get('/addAccount', (req, res) => {
+    const username = req.session.username;
+    res.render('addAccount', { loginName: username })
+})
+
+//Delete Account Function
+APP.get('/deleteAccount', async (req, res) => {
+    const idInput = req.query.id;
+    await deleteAccount(idInput);
+    res.redirect('manageAccounts');
+})
+
+//Search by ID to edit Account Function
+APP.get('/editAccount', async (req, res) => {
+    const idInput = req.query.id;
+    const username = req.session.username;
+    const search_Account = await getAccountById(idInput);
+    res.render('editAccount', { loginName: username, account: search_Account })
+})
+
+//Update account function
+APP.post('/updateAccount', async (req, res) => {
+    const id = req.body.id;
+    const usernameInput = req.body.txtUsername;
+    const passwordInput = req.body.txtPassword;
+    await updateAccount(id, usernameInput, passwordInput);
+    res.redirect('manageAccounts');
+})
+
+// End: Manage account
+
+//Login Function + Logout Function
+APP.get('/', (req, res) => {
+    res.render('login');
+})
+
+APP.post('/login', async (req, res) => {
+    const usernameInput = req.body.txtUsername;
+    const passwordInput = req.body.txtPassword;
+    const found = await checkUser(usernameInput, passwordInput)
+
+    if (found) {
+        req.session.username = usernameInput;
+        res.redirect('home');
+    }
+    else {
+        res.render('login', { errorMsg: "Username or password is incorrect. Please, try again!" });
+    }
+})
+
+
+const PORT = process.env.PORT || 5000;
+APP.listen(PORT);
---
+++ databaseHandler.js
@@ -0,0 +1,96 @@
+const { MongoClient, ObjectId } = require('mongodb');
+
+//const URL = 'mongodb://localhost:27017';
+const URL = "mongodb+srv://dbasm2:meocom255@cluster0.pcwuq.mongodb.net/test";
+
+const DATABASE_NAME = "DBASM2"
+
+//Get Database
+async function getDB() {
+    const client = await MongoClient.connect(URL);
+    const dbo = client.db(DATABASE_NAME);
+    return dbo;
+}
+
+//Add product function
+async function addProduct(newProduct) {
+    const dbo = await getDB();
+    const newS = await dbo.collection("products").insertOne(newProduct);
+    console.log("Gia tri id moi duoc insert la: ", newS.insertedId.toHexString());
+}
+
+//Delete product function
+async function deleteProduct(idInput) {
+    const dbo = await getDB();
+    await dbo.collection("products").deleteOne({ _id: ObjectId(idInput) });
+}
+
+//Search product funtion
+async function searchProduct(searchInput) {
+    const dbo = await getDB();
+    const allProducts = await dbo.collection("products").find({ name: searchInput }).toArray();
+    return allProducts;
+}
+
+//Get All products
+async function getAllProducts() {
+    const dbo = await getDB();
+    const allProducts = await dbo.collection("products").find({}).toArray();
+    return allProducts;
+}
+
+//Get Product by ID
+async function getProductById(idInput) {
+    const dbo = await getDB();
+    return dbo.collection("products").findOne({ _id: ObjectId(idInput) });
+}
+
+//Update product function
+async function updateProduct(id, nameInput, priceInput, imgURLInput) {
+    const dbo = await getDB();
+    dbo.collection("products").updateOne({ _id: ObjectId(id) }, { $set: { name: nameInput, price: priceInput, imgURL: imgURLInput } })
+}
+
+//Get All accounts
+async function getAllAccounts() {
+    const dbo = await getDB();
+    const allAccounts = await dbo.collection("accounts").find({}).toArray();
+    return allAccounts;
+}
+
+//Add account function
+async function addAccount(newAccount) {
+    const dbo = await getDB();
+    const newS = await dbo.collection("accounts").insertOne(newAccount);
+    console.log("Gia tri id moi duoc insert la: ", newS.insertedId.toHexString());
+}
+
+//Delete account function
+async function deleteAccount(idInput) {
+    const dbo = await getDB();
+    await dbo.collection("accounts").deleteOne({ _id: ObjectId(idInput) });
+}
+
+
+//Get Account by ID
+async function getAccountById(idInput) {
+    const dbo = await getDB();
+    return dbo.collection("accounts").findOne({ _id: ObjectId(idInput) });
+}
+
+//Update account function
+async function updateAccount(id, usernameInput, passwordInput) {
+    const dbo = await getDB();
+    dbo.collection("accounts").updateOne({ _id: ObjectId(id) }, { $set: { username: usernameInput, password: passwordInput } })
+}
+
+async function checkUser(usernameInput, passwordInput) {
+    const dbo = await getDB();
+    const results = await dbo.collection("accounts").findOne({ username: usernameInput, password: passwordInput  });
+    if (results != null)
+        return true;
+    else
+        return false;
+}
+
+module.exports = { getDB, addProduct, deleteProduct, searchProduct, getAllProducts, getProductById, updateProduct, getAllAccounts, addAccount, deleteAccount, getAccountById, updateAccount, checkUser }
